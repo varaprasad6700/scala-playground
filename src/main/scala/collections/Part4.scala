5b003a680e30a6ba42711f4c29e973b5ff87ee4c
varaprasad <varaprasad.p@techsophy.com>
2022-09-16 15:20:24 +0530

    changed Part4.scala

--- src/main/scala/collections/Part4.scala
+++ src/main/scala/collections/Part4.scala
@@ -3,12 +3,15 @@ package collections
 
 object Part4 extends App {
   val numbers = List(1, 2, 3, 4, 5)
+  val mapFunction = (x: Int) => 2 * x
+  val flatMapFunction = (n: Int) => List.fill(2)(n)
   //  PART 4
   //  1. Implement map using flatMap
-  println(numbers.flatMap(n => List(List.fill(2)(n))))
+  println(numbers.flatMap(n => List(mapFunction(n))))
   //  2. Implement map using foldLeft
-  numbers.foldLeft[List[Int]](List.empty[Int])((li, ele) => ele :: li)
+  println(numbers.foldLeft[List[Int]](List.empty[Int])((acc, x) => mapFunction(x) :: acc).reverse)
   //  3. Implement flatMap using foldLeft
+  println(numbers.foldLeft[List[Int]](List.empty[Int])((acc, x) => flatMapFunction(x) ::: acc).reverse)
   //  4. Implement reduceLeft using foldLeft
-//  Second Minimum element from a list
+  println(numbers.tail.foldLeft(numbers.head)(_ + _))
 }
